633dc4fe521cb9ecd2e62f57566e588c5456b226
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-10-06 18:19:07 -0300

    Update

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -79,7 +79,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       }
 
     } catch {
-      toast.error('Erro na remoção do produto')
+      toast.error('Erro na remoção do produto');
     }
   };
 
